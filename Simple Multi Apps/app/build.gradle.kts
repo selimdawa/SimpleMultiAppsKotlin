e0babe7dddbdae84588b56d071476d23f14f3488
selimdawa <selimdawa@gmail.com>
2026-04-10 02:34:54 +0300

    ADD minorApiLevel

--- Simple Multi Apps/app/build.gradle.kts
+++ Simple Multi Apps/app/build.gradle.kts
@@ -7,7 +7,9 @@ plugins {
 android {
     namespace = "com.flatcode.simplemultiapps"
     compileSdk {
-        version = release(36)
+        version = release(36) {
+            minorApiLevel = 1
+        }
     }
 
     defaultConfig {
