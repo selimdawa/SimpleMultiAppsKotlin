bf7101359f482377ec919ca32dc0b52bfd3184e2
selimdawa <selimdawa@gmail.com>
2026-06-09 02:45:19 +0300

    SDK 37 / Android 17

--- Simple Multi Apps/app/build.gradle.kts
+++ Simple Multi Apps/app/build.gradle.kts
@@ -7,15 +7,13 @@ plugins {
 android {
     namespace = "com.flatcode.simplemultiapps"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.flatcode.simplemultiapps"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 7
         versionName = "1.35"
 
